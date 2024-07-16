f0e8b9b41528df35ebbac8ed2489019f042b52d8
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-16 13:32:44 +1000

    func: implement mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-import Image from "next/image";
+import  { getDonts } from "@/actions/action";
+import dbConnect from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  await dbConnect();
+  const donts = await getDonts();
+  console.log(donts);
+
   return (
     <main className="">
       
