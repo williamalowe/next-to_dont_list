e85008fefd0d2b7d16167e245a8da8ac20c45e15
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-16 13:49:30 +1000

    feat: add background component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
 
   return (
     <main className="">
-      
+      <h1>Hello World</h1>
     </main>
   );
 }
---
+++ src/components/background.tsx
@@ -0,0 +1,5 @@
+export default function Background() {
+  return (
+    <div className="fixed inset-0 -z-10 h-full w-full bg-white-300 bg-[linear-gradient(to_right,#00000025_1px,transparent_1px),linear-gradient(to_bottom,#00000025_1px,transparent_1px)] bg-[size:6rem_3rem]" />
+  );
+}
