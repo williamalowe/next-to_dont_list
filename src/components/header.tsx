e6d19c7c657f07c85d6e753e9f6b110180d0b361
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-16 16:57:56 +1000

    stlyes: update header stylings

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,8 +8,8 @@ const waiting_for_the_sunrise = Waiting_for_the_Sunrise({
 export default function Header() {
   return (
     <div className={`${waiting_for_the_sunrise.className} relative`}>
-      <h1 className="text-center text-8xl">To <span className="line-through">Do</span> List</h1>
-      <h1 className="absolute text-center text-6xl -top-12 left-[25%] rotate-12 underline">Don&apos;t</h1>
+      <h1 className="font-extrabold text-center text-8xl">To <span className="line-through">Do</span> List</h1>
+      <h1 className="font-extrabold absolute text-center text-6xl -top-12 left-[25%] rotate-12 underline">Don&apos;t</h1>
     </div>
   )
 }
