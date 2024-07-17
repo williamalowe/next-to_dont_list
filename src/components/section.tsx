9f27ee464c875b71f158f390c58087cee0318c37
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-17 13:26:59 +1000

    func: correct typing added for section component

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,12 +1,18 @@
+type Dont = {
+  _id: string,
+  dont: string,
+  score: number
+}
+
 export default function Section({ header, listItems }: {
   header: string,
-  listItems: any
+  listItems: Dont[]
 }) {
   return (
     <section className="w-full flex flex-col items-left">
       <h3 className="text-6xl font-bold">{header}</h3>
       <ul className="mt-4 flex flex-col gap-y-2">
-        {listItems.map((dont) => 
+        {listItems.map((dont: Dont) => 
         <li key={dont._id} className="ml-6 list-disc">
           <p className="text-4xl p-2 bg-orange-300 rounded-full">{dont.dont} <span className={`${dont.score < 0 ? 'text-red-600' : dont.score > 0 ? 'text-green-600' : 'text-black/50'} font-bold ml-auto`}>{dont.score}</span></p>
         </li>
