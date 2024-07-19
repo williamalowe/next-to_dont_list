78772632022a58d3804cacaec57ace23abcc6cef
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-19 13:56:37 +1000

    styles: update background styles

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,7 +1,7 @@
 export default function Background() {
   return (
     <div className="fixed mt-24 inset-0 -z-10 h-full w-full bg-white-300 bg-[linear-gradient(to_right,#7dd3fc_1px,transparent_1px),linear-gradient(to_bottom,#7dd3fc_1px,transparent_1px)] bg-[size:100vw_2rem]">
-      <div className="fixed top-0 left-24 h-full w-[2px] bg-red-400"></div>
+      <div className="fixed top-0 left-12 sm:left-24 h-full w-[2px] bg-red-400"></div>
     </div>
   );
 }
